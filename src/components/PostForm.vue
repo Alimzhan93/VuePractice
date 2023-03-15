92e1ff1e1496df9ae82a43c0c604482aa9a8420d
77001 <alimzhanzhakashev@gmail.com>
2023-03-15 15:09:07 +0600

    commit innit

---
+++ src/components/PostForm.vue
@@ -0,0 +1,54 @@
+<template>
+  <form @submit.prevent>
+    <h4>Создание поста</h4>
+    <MyInput v-focus v-model="post.title" type="text" placeholder="Название" />
+    <MyInput v-model="post.body" type="text" placeholder="Описание" />
+
+    <MyButton
+      style="align-self: flex-end; margin-top: 15px"
+      @click="createPost"
+    >
+      Создать
+    </MyButton>
+  </form>
+</template>
+
+<script>
+import MyButton from "./UI/MyButton.vue";
+import MyInput from "./UI/MyInput.vue";
+
+export default {
+  data() {
+    return {
+      post: {
+        title: "",
+        body: "",
+      },
+    };
+  },
+  methods: {
+    createPost() {
+      this.post.id = Date.now();
+      this.$emit("create", this.post);
+      this.post = {
+        title: "",
+        body: "",
+      };
+    },
+  },
+  components: { MyButton, MyInput },
+};
+</script>
+
+<style scoped>
+form {
+  display: flex;
+  flex-direction: column;
+}
+/* .input {
+  width: 100%;
+  border: 1px solid teal;
+  padding: 10px 15px;
+  margin-top: 15px;
+} */
+</style>
---
+++ src/components/PostList.vue
@@ -0,0 +1,28 @@
+<template>
+  <div v-if="posts.length > 0">
+    <h3>Список пользователей</h3>
+    <Postitem
+      v-for="post in posts"
+      :post="post"
+      :key="post.id"
+      @remove="$emit('remove', post)"
+    />
+  </div>
+  <h2 v-else style="color: red">Список пользователей пуст</h2>
+</template>
+
+<script>
+import Postitem from "@/components/Postitem.vue";
+
+export default {
+  props: {
+    posts: {
+      type: Array,
+      required: true,
+    },
+  },
+  components: { Postitem },
+};
+</script>
+
+<style></style>
